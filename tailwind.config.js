90ca62833633e11ee09052b7ea274f91281fa7ef
Kyoungjun Han <kjers96@gmail.com>
2023-03-10 18:34:15 +0900

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+/** This is where you can come up with your own configs. */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
