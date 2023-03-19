564ea2daa44ea83b224c542e7953ae3bf7e6048b
Kyoungjun Han <kjers96@gmail.com>
2023-03-19 15:16:30 +0900

    image map prototype implemented. Read component edited so that it takes props from outside componenets

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,16 +2,39 @@ import React, { useState } from 'react';
 import Modal from 'react-modal';
 import Read from './Read';
 import AmazonMap from '../Images/AmazonMap.png'; // import the image here
+import '../Styles.css';
+
+interface HomeProps {
+  origin: string;
+};
 
 const Home = () => {
   const [accordionIsOpen, setAccordionIsOpen] = useState(false);
+  const [transform, setTransform] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedOrigin, setSelectedOrigin] = useState('all');
+
+  const openModal = (origin: string) => {
+    setSelectedOrigin(origin);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   const toggleAccordion = () => {
     setAccordionIsOpen(!accordionIsOpen);
   };
 
+  const handleClick = () => {
+    console.log("clicked");
+  }
+
   return (
     <>
+    <div className="bg-gray-100">
       <div className="flex flex-col items-center justify-center py-6" >
         <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded transition-all duration-500" type="button" onClick={toggleAccordion}>
           {accordionIsOpen ? 'Hide' : 'Click for Info'}
@@ -19,7 +42,7 @@ const Home = () => {
       </div>
 
       <div style={{ maxHeight: accordionIsOpen ? '1000px' : '0', overflow: 'hidden', transition: 'max-height 0.5s ease-in-out' }}>
-        <div className="bg-white p-6 w-full items-center justify-center relative flex flex-col mt-4">
+        <div className="bg-white p-6 w-full items-center justify-center relative flex flex-col mt-4 shadow-lg rounded-lg">
           <div className="text-lg font-serif flex-1 mb-4">
             <p>
               Apistogramma is a genus of freshwater fish in the cichlid family, native to South America. These fish are popular in the aquarium hobby due to their beautiful colors and interesting behavior.
@@ -37,9 +60,16 @@ const Home = () => {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center">
-      <Read />
-      <img src={AmazonMap} alt="Amazon Map" />
+      <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded" onClick={() => openModal('all')}>View All Apistogrammas</button>
+      {isModalOpen && (
+        <Read origin={selectedOrigin} closeModal={closeModal} />
+      )}
+      <img src={AmazonMap} alt="Amazon Map" useMap="#amazonMap" className="rounded-lg shadow-lg py-4" />
+      <map name="amazonMap">
+        <area shape="rect" coords="280, 345, 600, 560" onMouseOver={() => setTransform(true)} onMouseOut={() => setTransform(false)} style={transform ? { transform: 'scale(1.2)', cursor: 'pointer' } : { cursor: 'default' }} onClick={() => openModal('all')} />
+      </map>
       </div>
+    </div>
     </>
   );
 
--- src/Components/Read.tsx
+++ src/Components/Read.tsx
@@ -4,6 +4,11 @@ import Modal from 'react-modal';
 import axios from 'axios';
 
 
+interface ReadProps {
+  origin: string;
+  closeModal: () => void;
+}
+
 interface Fish {
   id: number;
   name: string;
@@ -11,29 +16,25 @@ interface Fish {
   size: string;
   pH: string;
   image: string;
-}
+};
 
-const Read = () => {
+const Read: React.FC<ReadProps> = ({ origin, closeModal }) => {
   const [fish, setFish] = useState<Fish[]>([]);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedFishId, setSelectedFishId] = useState<number | null>(null);
   const navigate = useNavigate();
 
+  const fetchFish = async (origin?: string) => {
+    let url = 'http://localhost:4000/all';
+    if (origin && origin !== 'all') {
+      url = `http://localhost:4000/origin?origin=${origin}`;
+    }
+    const res = await axios.get<Fish[]>(url);
+    setFish(res.data);
+  };
+
   useEffect(() => {
-    const fetchFish = async () => {
-      const res = await axios.get<Fish[]>('http://localhost:4000/all');
-      setFish(res.data);
-    };
-    fetchFish();
-  }, []);
-
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
+    fetchFish(origin);
+  }, [origin]);
 
   const handleFishClick = (id: number) => {
     if (id === selectedFishId) {
@@ -59,20 +60,11 @@ const Read = () => {
 
   return (
     <>
-      <div className="flex justify-center">
-        <button
-          className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
-          type="button"
-          onClick={openModal}
-        >
-          View All Apistogrammas
-        </button>
-      </div>
-
       <Modal
-        isOpen={modalIsOpen}
+        isOpen={true}
         contentLabel="Apistogramma Fish"
         className="fixed inset-0 flex items-center justify-center"
+        onRequestClose={closeModal}
       >
         <div
           className="bg-white rounded-lg shadow-lg p-6 w-1/2 h-1/2 relative flex flex-col"
@@ -82,33 +74,35 @@ const Read = () => {
             <h2>Apistogramma Fish</h2>
           </div>
           <div className="flex-1 mb-4">
-            {fish.map((fish) => (
-              <button
-                key={fish.id}
-                className={`flex flex-row justify-center items-center mt-6 shadow-md bg-gray-100 hover:bg-gray-200 ${
-                  selectedFishId === fish.id ? 'bg-gray-200' : ''
-                }`}
-                onClick={() => handleFishClick(fish.id)}
-              >
-                <div className="w-1/2 p-6">
-                  <img
-                    src={fish.image}
-                    alt="Fish"
-                    className="w-300 h-300 rounded-full"
-                  />
-                </div>
-                <div className="w-1/2 p-6">
-                  <h2 className="text-xl font-bold mb-2">{fish.name}</h2>
-                  <p className="text-gray-700 text-base">
-                    Origin: {fish.origin}
-                  </p>
-                  <p className="text-gray-700 text-base">
-                    Size: {fish.size}cm
-                  </p>
-                  <p className="text-gray-700 text-base">pH: {fish.pH}</p>
-                </div>
-              </button>
-            ))}
+          {fish
+  .filter((fish) => origin === 'all' || fish.origin === origin)
+  .map((fish) => (
+    <button
+      key={fish.id}
+      className={`flex flex-row justify-center items-center mt-6 shadow-md bg-gray-100 hover:bg-gray-200 ${
+        selectedFishId === fish.id ? 'bg-gray-200' : ''
+      }`}
+      onClick={() => handleFishClick(fish.id)}
+    >
+      <div className="w-1/2 p-6">
+        <img
+          src={fish.image}
+          alt="Fish"
+          className="w-300 h-300 rounded-full"
+        />
+      </div>
+      <div className="w-1/2 p-6">
+        <h2 className="text-xl font-bold mb-2">{fish.name}</h2>
+        <p className="text-gray-700 text-base">
+          Origin: {fish.origin}
+        </p>
+        <p className="text-gray-700 text-base">
+          Size: {fish.size}cm
+        </p>
+        <p className="text-gray-700 text-base">pH: {fish.pH}</p>
+      </div>
+    </button>
+  ))}
           </div>
           <div className="flex justify-center">
             <button
